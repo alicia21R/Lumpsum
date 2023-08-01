ecda2c09c17bd7883e739d38a9ef0623cad319d9
alicia kasemiire <aliciakasemiire@gmail.com>
2023-08-01 19:00:09 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <div className="text-3xl font-bold underline">
+      <p>Back at it again</p>
+      </div>
+    </>
+  )
+}
+
+export default App
